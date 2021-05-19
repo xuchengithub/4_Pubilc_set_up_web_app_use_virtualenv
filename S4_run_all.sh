ab0d7dcc54a93a2a82abbc50d1dfd4e312f61c03
xuchen <xuchen1522@gmail.com>
2021-05-19 19:42:24 +0900

    change_s4

--- S4_run_all.sh
+++ S4_run_all.sh
@@ -1,4 +1,4 @@
-S0_set_up_git.sh
-S1_set_up_env.sh
-S2_download_web_frame.sh
-S3_run_to_build_web_frame.sh
+source S0_set_up_git.sh
+source S1_set_up_env.sh
+source S2_download_web_frame.sh
+source S3_run_to_build_web_frame.sh
