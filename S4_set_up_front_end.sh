484549df3b7de22600d9abbf0ffb624837e079e9
xu chen <xuchen1522@gmail.com>
2021-05-26 03:49:08 +0900

    Update S4_set_up_front_end.sh

--- S4_set_up_front_end.sh
+++ S4_set_up_front_end.sh
@@ -1,5 +1,12 @@
 cd ..
-npx create-react-app react-front-end
 
 #curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 #sudo apt-get install -y nodejs
+
+npx create-react-app react-front-end
+cd react-front-end
+npm run eject
+
+#change config/paths.js/  =>
+
+
